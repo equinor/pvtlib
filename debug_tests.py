cb744ccd41e95eb0805f368e2cd93c870e510adc
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-01-07 13:52:41 +0100

    Update debug_tests.py

--- debug_tests.py
+++ debug_tests.py
@@ -1,7 +1,7 @@
 from tests.test_metering import *
 
 if __name__ == '__main__':
-    #test_calculate_flow_venturi()
+    test_calculate_flow_venturi()
     test_calculate_expansibility_ventiruri()
     test_calculate_beta_V_cone()
     test_calculate_expansibility_Stewart_V_cone()
