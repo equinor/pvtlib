a153c6ac3adc8e4f69a6f021bb680e57c5610f5c
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-01-12 11:55:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,19 +28,19 @@ setup(
     version='1.1.0',
     author='Christian Hågenvik',
     author_email='chaagen2013@gmail.com',
-    description='A library containing various tools in the cathegories of thermodynamics, fluid mechanics, metering etc.',
+    description='A library containing various tools in the categories of thermodynamics, fluid mechanics, metering etc.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/chagenvik/pvtlib',
     packages=find_packages(),
     install_requires=[
         'numpy',
-        'pandas',
     ],
+    license='MIT',  # Add this line to specify the license
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
 )
