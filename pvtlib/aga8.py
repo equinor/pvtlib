36b44b59a2209ebd4b8d60adb24a082b110c25e4
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-02-19 10:37:01 +0100

    Update aga8.py

--- pvtlib/aga8.py
+++ pvtlib/aga8.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-The code is based upon the python library pyaga8, which is again based upon the open source rust package called aga8 (https://crates.io/crates/aga8)
+The code is based upon the python library pyaga8, which is again based upon the open source rust package aga8 (https://crates.io/crates/aga8)
 """
 
 import pyaga8
