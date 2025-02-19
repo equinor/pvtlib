74dab1b78128a36bb680138649b4c8cc63af3768
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-02-19 10:36:47 +0100

    Update aga8.py

--- pvtlib/aga8.py
+++ pvtlib/aga8.py
@@ -19,6 +19,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+The code is based upon the python library pyaga8, which is again based upon the open source rust package called aga8 (https://crates.io/crates/aga8)
 """
 
 import pyaga8
