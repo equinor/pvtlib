1c6e60a5cd5f132c136b33e5d07873d2f7f822fc
Christian Hågenvik <55736321+chagenvik@users.noreply.github.com>
2025-05-10 11:54:19 +0200

    add linear interpolation

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -22,7 +22,31 @@ SOFTWARE.
 """
 
 import numpy as np
-from pvtlib.utilities import relative_difference, calculate_deviation, calculate_relative_deviation, calculate_max_min_diffperc
+from pvtlib.utilities import linear_interpolation, relative_difference, calculate_deviation, calculate_relative_deviation, calculate_max_min_diffperc
+
+def test_linear_interpolation():
+    X = [100, 300, 900, 1400, 1900, 2500, 3100]
+    Y = [-0.09, -0.05, 0.1, 0.15, 0.16, 0.14, 0.05]
+
+    testdata = {
+        'test1' : {'x': 0, 'expected': -0.09},
+        'test2' : {'x': 150, 'expected': -0.08},
+        'test3' : {'x': 200, 'expected': -0.07},
+        'test4' : {'x': 500, 'expected': 0.00},
+        'test5' : {'x': 1000, 'expected': 0.11},
+        'test6' : {'x': 1500, 'expected': 0.152},
+        'test7' : {'x': 2000, 'expected': 0.1566666667},
+        'test8' : {'x': 2500, 'expected': 0.14},
+        'test9' : {'x': 3000, 'expected': 0.065},
+        'test10' : {'x': 3500, 'expected': 0.05},
+    }
+    
+    for key, value in testdata.items():
+        x = value['x']
+        expected = value['expected']
+        result = linear_interpolation(x, X, Y)
+        assert round(result, 10) == expected, f"Test {key} failed: expected {expected}, got {result}"
+
 
 def test_relative_difference():
     assert round(relative_difference(10, 5),10)== 66.6666666667
